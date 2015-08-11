f7e7e3d62c7b7284019c252aa6c415f127e93754
anson.zhenglei <anson@163.com>
2015-08-11 15:38:34 +0800

    firstproject

---
+++ helloworld/helloworld/helloworldDlg.h
@@ -0,0 +1,32 @@
+
+// helloworldDlg.h : header file
+//
+
+#pragma once
+
+
+// ChelloworldDlg dialog
+class ChelloworldDlg : public CDialogEx
+{
+// Construction
+public:
+	ChelloworldDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+	enum { IDD = IDD_HELLOWORLD_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
